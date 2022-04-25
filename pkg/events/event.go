d014f7303b2019f6db2b080da622e9408209cf1e
brent shierk <brentshierk@brents-MacBook-Pro.local>
2022-04-25 16:31:44 -0700

    cli-chat

---
+++ pkg/events/event.go
@@ -0,0 +1,21 @@
+package events
+
+
+type EventType int8
+
+const (
+	ConnectEvent    EventType = iota // Has the value 0.
+	DisconnectEvent                  // Has the value 1.
+)
+
+type Event struct {
+	Type EventType
+	User *User
+}
+
+func NewEvent(_type EventType, user *User) *Event {
+	return &Event{
+		Type: _type,
+		User: user,
+	}
+}
---
+++ pkg/payload/payload.go
@@ -0,0 +1,7 @@
+package payload
+
+
+type Payload struct {
+	Message
+	Notification
+}
